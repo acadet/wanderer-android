3d8c443880ddd3e44375ad916b4ded7efcdced56
Adrien Cadet <acadet@live.fr>
2016-03-30 09:56:41 -0700

    Implements ListPlacesByVisitDateDescJob

---
+++ app/src/main/java/com/adriencadet/wanderer/models/services/wanderer/jobs/ListPlacesByVisitDateDescJob.java
@@ -0,0 +1,42 @@
+package com.adriencadet.wanderer.models.services.wanderer.jobs;
+
+import com.adriencadet.wanderer.models.services.RetrofitJob;
+import com.adriencadet.wanderer.models.services.wanderer.api.IWandererAPI;
+import com.adriencadet.wanderer.models.services.wanderer.dto.PlaceWandererServerDTO;
+
+import java.util.List;
+
+import retrofit.RetrofitError;
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+/**
+ * ListPlacesByVisitDateDescJob
+ * <p>
+ */
+public class ListPlacesByVisitDateDescJob extends RetrofitJob {
+    private Observable<List<PlaceWandererServerDTO>> observable;
+
+    ListPlacesByVisitDateDescJob(IWandererAPI api) {
+        observable = Observable
+            .create(new Observable.OnSubscribe<List<PlaceWandererServerDTO>>() {
+                @Override
+                public void call(Subscriber<? super List<PlaceWandererServerDTO>> subscriber) {
+                    try {
+                        List<PlaceWandererServerDTO> outcome = api.listPlacesByVisitDateDesc();
+
+                        subscriber.onNext(outcome);
+                        subscriber.onCompleted();
+                    } catch (RetrofitError e) {
+                        handleError(e, subscriber);
+                    }
+                }
+            })
+            .subscribeOn(Schedulers.newThread());
+    }
+
+    public Observable<List<PlaceWandererServerDTO>> create() {
+        return observable;
+    }
+}
