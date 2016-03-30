090070f8020459f8328c3b69226ef937ee912d96
Adrien Cadet <acadet@live.fr>
2016-03-29 18:39:35 -0700

    Adds base resources

--- app/src/main/java/com/adriencadet/wanderer/ui/MainActivity.java
+++ app/src/main/java/com/adriencadet/wanderer/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.adriencadet.wanderer;
+package com.adriencadet.wanderer.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.adriencadet.wanderer.R;
 
 public class MainActivity extends AppCompatActivity {
 
