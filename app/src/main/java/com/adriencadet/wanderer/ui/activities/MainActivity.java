ba9b532ff0f4463980b9f5061251efb05562c7ed
Adrien Cadet <acadet@live.fr>
2016-03-30 18:24:00 -0700

    Creates base layouts

--- app/src/main/java/com/adriencadet/wanderer/ui/activities/MainActivity.java
+++ app/src/main/java/com/adriencadet/wanderer/ui/activities/MainActivity.java
@@ -1,15 +1,38 @@
 package com.adriencadet.wanderer.ui.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 
 import com.adriencadet.wanderer.R;
+import com.adriencadet.wanderer.ui.components.MainUIContainer;
+import com.adriencadet.wanderer.ui.screens.PlaceListScreen;
+import com.lyft.scoop.Scoop;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+    @Bind(R.id.main_ui_container)
+    MainUIContainer container;
+
+    private Scoop rootScoop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rootScoop = new Scoop.Builder("root").build();
+        rootScoop.inflate(R.layout.root_layout, (ViewGroup) findViewById(R.id.main_layout), true);
+        ButterKnife.bind(this);
+
+        appRouter.goTo(new PlaceListScreen());
     }
+
+    //    @Override
+    //    public void onBackPressed() {
+    //        if (!appRouter.goBack()) {
+    //            finish();
+    //        }
+    //    }
 }
