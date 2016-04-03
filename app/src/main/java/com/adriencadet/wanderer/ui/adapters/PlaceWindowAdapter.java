bf22db42df142e4e3460a73759dd477760b7dd18
Adrien Cadet <acadet@live.fr>
2016-04-02 20:19:01 -0700

    Adds custom info window for map markers

---
+++ app/src/main/java/com/adriencadet/wanderer/ui/adapters/PlaceWindowAdapter.java
@@ -0,0 +1,59 @@
+package com.adriencadet.wanderer.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.adriencadet.wanderer.R;
+import com.adriencadet.wanderer.models.bll.dto.PlaceBLLDTO;
+import com.adriencadet.wanderer.ui.helpers.DateFormatterHelper;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * PlaceWindowAdapter
+ * <p>
+ */
+public class PlaceWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    private Context     context;
+    private PlaceBLLDTO item;
+
+    static class ViewHolder {
+        @Bind(R.id.adapter_place_window_name)
+        TextView name;
+
+        @Bind(R.id.adapter_place_window_date)
+        TextView date;
+    }
+
+    public PlaceWindowAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null; // On purpose
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        View view;
+        ViewHolder viewHolder = new ViewHolder();
+
+        view = LayoutInflater.from(context).inflate(R.layout.adapter_place_window, null, false);
+        ButterKnife.bind(viewHolder, view);
+
+        viewHolder.name.setText(item.getName());
+        viewHolder.date.setText(DateFormatterHelper.userFriendy(item));
+
+        return view;
+    }
+
+    public void setItem(PlaceBLLDTO item) {
+        this.item = item;
+    }
+}
--- app/src/main/java/com/adriencadet/wanderer/ui/controllers/PlaceMapController.java
+++ app/src/main/java/com/adriencadet/wanderer/ui/controllers/PlaceMapController.java
@@ -4,18 +4,37 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 
 import com.adriencadet.wanderer.R;
+import com.adriencadet.wanderer.models.bll.dto.PlaceBLLDTO;
 import com.adriencadet.wanderer.ui.activities.BaseActivity;
+import com.adriencadet.wanderer.ui.adapters.PlaceWindowAdapter;
+import com.adriencadet.wanderer.ui.screens.PlaceInsightScreen;
+import com.annimon.stream.Stream;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
+
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 
 /**
  * PlaceMapController
  * <p>
  */
-public class PlaceMapController extends BaseController implements OnMapReadyCallback {
+public class PlaceMapController extends BaseController implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener {
+    private Subscription listPlacesByVisitDateDescSubscription;
+
+    private PlaceWindowAdapter       placeWindowAdapter;
+    private Map<Marker, PlaceBLLDTO> markerPlaceHash;
 
     @Inject
     FragmentManager fragmentManager;
@@ -31,7 +50,10 @@ public class PlaceMapController extends BaseController implements OnMapReadyCall
 
         BaseActivity.getFragmentComponent().inject(this);
 
-        MapFragment fragment = MapFragment.newInstance();
+        GoogleMapOptions mapOptions = new GoogleMapOptions()
+            .compassEnabled(false)
+            .rotateGesturesEnabled(false);
+        MapFragment fragment = MapFragment.newInstance(mapOptions);
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(R.id.place_map, fragment);
         transaction.commit();
@@ -39,7 +61,58 @@ public class PlaceMapController extends BaseController implements OnMapReadyCall
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onDetach() {
+        super.onDetach();
 
+        if (listPlacesByVisitDateDescSubscription != null) {
+            listPlacesByVisitDateDescSubscription.unsubscribe();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        placeWindowAdapter = new PlaceWindowAdapter(context);
+
+        googleMap.setInfoWindowAdapter(placeWindowAdapter);
+        googleMap.setOnMarkerClickListener(this);
+        googleMap.setOnInfoWindowClickListener(this);
+
+        listPlacesByVisitDateDescSubscription = dataReadingBLL
+            .listPlacesByVisitDateDesc()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new BaseSubscriber<List<PlaceBLLDTO>>() {
+                @Override
+                public void onCompleted() {
+
+                }
+
+                @Override
+                public void onNext(List<PlaceBLLDTO> placeBLLDTOs) {
+                    markerPlaceHash = new HashMap<>();
+
+                    Stream
+                        .of(placeBLLDTOs)
+                        .forEach((e) -> {
+                            markerPlaceHash.put(
+                                googleMap.addMarker(
+                                    new MarkerOptions()
+                                        .position(new LatLng(e.getLatitude(), e.getLongitude()))
+                                ),
+                                e
+                            );
+                        });
+                }
+            });
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        placeWindowAdapter.setItem(markerPlaceHash.get(marker));
+        return false;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        appRouter.goTo(new PlaceInsightScreen(markerPlaceHash.get(marker)));
     }
 }
