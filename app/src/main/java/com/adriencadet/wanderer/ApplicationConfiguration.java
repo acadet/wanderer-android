b8defe80507380a494c5ab97c1383b97c33c4409
Adrien Cadet <acadet@live.fr>
2016-04-04 09:30:49 -0700

    Styles place insight

--- app/src/main/java/com/adriencadet/wanderer/ApplicationConfiguration.java
+++ app/src/main/java/com/adriencadet/wanderer/ApplicationConfiguration.java
@@ -5,7 +5,7 @@ package com.adriencadet.wanderer;
  * <p>
  */
 public class ApplicationConfiguration {
-    public final String WANDERER_SERVER_ENDPOINT = "http://www49647.coshx.com/";
+    public final String WANDERER_SERVER_ENDPOINT = "http://www51120.coshx.com/";
 
     public final int PLACE_CACHING_DURATION_MINS     = 1;
     public final int PLACE_MAX_CACHING_DURATION_MINS = 10;
